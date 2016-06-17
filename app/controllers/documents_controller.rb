2f8ba0db58aad1b6efe9ddf748dd60a9269d2886
gotrecillo <sergio.panadero.perez@gmail.com>
2016-06-17 16:11:00 +0200

    add document working

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,29 +1,53 @@
 class DocumentsController < ApplicationController
+  before_action :find_document, only: [:show, :edit, :update, :destroy]
 
   def index
+    @documents = Document.all.order("created_at DESC")
   end
 
   def show
+    @document = find_document
+  end
+
+  def new
+    @document = Document.new
   end
 
   def create
+    @document = Document.new(document_params)
+
+    if @document.save
+      redirect_to @document
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    if @document.update
+      redirect_to @document
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    if @document.destroy
+      redirect_to documents_path
+    end
   end
 
   private
-  
+
     def find_document
+      @document = Document.find(params[:id])
     end
 
     def document_params
+      params.require(:document).permit(:title, :content)
     end
 
 end
