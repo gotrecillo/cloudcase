9cef62dbe229ce4149742e29d377e0cecd3b1a26
gotrecillo <sergio.panadero.perez@gmail.com>
2016-06-20 15:27:21 +0200

    association between users and documents in the controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160620130458) do
+ActiveRecord::Schema.define(version: 20160620131842) do
 
   create_table "documents", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
